58b8e5535f4e05c5c6286f7b15b4e651fc26d14d
Andy Lulham <a.lulham@gmail.com>
2014-01-16 19:14:17 +0000

    Add some comments to project page js

--- js/projects.js
+++ js/projects.js
@@ -5,12 +5,16 @@ jQuery(document).ready(function($) {
 
   // triggered when a form filter is clicked
   $("form#filters").on('click', 'button', function() {
+    // toggle the button state
     $(this).button('toggle');
+
+    // make a list of all the currently active filters
     var filter_arr = [];
     $("form#filters button.active").each(function() {
       filter_arr.push('[data-' + $(this).data('filter') + ']');
     });
 
+    // filter projects
     container.isotope({
       filter: filter_arr.join('')
     });
@@ -24,6 +28,7 @@ jQuery(document).ready(function($) {
     var url = record.data('url');
     record.addClass('expand');
 
+    // de-duplicate filter values
     $.each(filters, function (filter_type) {
       var filter_vals = record.data(filter_type).split(';');
       $.each(filter_vals, function(ignore, filter_val) {
@@ -48,8 +53,11 @@ jQuery(document).ready(function($) {
     });
   });
 
+  // create filter buttons
   $.each(filters, function(filter_type, filter) {
     $.each(filter, function(k, v) {
+      // the '*=' here is a hack. if a filter value is a substring of
+      // another (e.g. java and javascript) this will go wrong.
       $('.btn-group.' + filter_type).append('<button type="button" class="btn btn-primary" data-filter="' + filter_type + '*=' + v + '">' + v + '</button>');
     });
   });
